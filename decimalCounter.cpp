fae4de33523cb5230f8dfb106a5e8188392138eb
FrankSteps <franciscopassos.contato@gmail.com>
2026-02-06 17:54:19 -0300

    Improve OOP structure by removing loop from main

--- decimalCounter.cpp
+++ decimalCounter.cpp
@@ -4,26 +4,22 @@
 #include <chrono>
 #include <thread>
 
-
-[[noreturn]] void CriticalError(const std::string& msg){
-    std::cerr << msg << std::endl;
-    std::exit(EXIT_FAILURE);
-}
-
-
-struct Converter{
-    void toBinary(unsigned int decimal, int bits = 10){
-        if(bits >=  0 && bits <= 10){
-            for(int i = bits-1; i >= 0; i--){
-                std::cout << ((decimal >> i) & 1);
-            }
-            std::cout << std::endl;
-        }else{
-            CriticalError("error :: reset Qpin isn't correct. Please, put a pin between 0 and 10");
-        }
+struct ErrorMsg{
+    [[noreturn]] static void CriticalError(const std::string& msg){
+        std::cerr << msg << std::endl;
+        std::exit(EXIT_FAILURE);
     }
 };
 
+
+struct Converter{
+    static void toBinary(unsigned int decimal, int bits){
+        for(int i = bits-1; i >= 0; i--){
+            std::cout << ((decimal >> i) & 1);
+        }
+        std::cout << std::endl;
+    }
+};
 
 
 class Chip4017{
@@ -32,15 +28,17 @@ class Chip4017{
         unsigned int LimitReset;
 
     public:
-        Chip4017(double limitReset) : LimitReset(limitReset) {};
+        explicit Chip4017(unsigned int limitReset) : LimitReset(limitReset) {
+            if(limitReset == 0 || limitReset > 10){
+                throw std::invalid_argument("Reset pin must be between 1 and 10");
+            }
+        };
 
         void shift(){
-            if(LimitReset <= 10){   //comparison of unsigned expression in ‘>= 0’ is always true
-                if(Out >= (1u << (LimitReset-1))){
-                    reset();
-                } else {
-                    Out = Out << 1;
-                }
+            if(Out >= (1u << (LimitReset-1))){
+                reset();
+            } else {
+                Out = Out << 1;
             }
         }
 
@@ -60,10 +58,6 @@ class Chip555{
         double R1, R2, C;
         const double Ln2 = 0.693;
 
-    public:
-        Chip555(double r1, double r2, double c ) 
-            : R1(r1), R2(r2), C(c) {};
-
         double tHigh(){
             return Ln2*(R1 + R2)*C;
         }
@@ -72,10 +66,13 @@ class Chip555{
             return Ln2*R2*C;
         }
 
-        bool clock(){
+        void clock(){
             output = !output;
-            return output;
         }
+
+    public:
+        Chip555(double r1, double r2, double c ) 
+            : R1(r1), R2(r2), C(c) {};
 
         void pulse(){
             clock();
@@ -86,18 +83,38 @@ class Chip555{
 };
 
 
+class runBoard{
+    private:
+        Chip4017& chip4017;
+        Chip555&  chip555;
+        unsigned int resetQP = 0;
+
+    public:
+        runBoard (Chip555& c555, Chip4017& c4017, unsigned int resetQP)
+        : chip4017(c4017), chip555(c555), resetQP(resetQP) {}
+
+        void run(){
+            while(true){
+                Converter::toBinary(chip4017.getOut(), resetQP);
+                chip555.pulse();
+                chip4017.shift(); 
+            }
+        }
+};
+
+
 int main(){ 
-    const int resetQP = 10;
+    try{
+        const int resetQP = 4;
 
-    Chip4017 chip4017(resetQP);
-    Chip555 chip555(1000, 10000, 7.37e-6);
-    Converter converter;
+        Chip4017 chip4017(resetQP);
+        Chip555 chip555(1000, 10000, 7.37e-6);
 
-    // cycle 
-    while(true){
-        converter.toBinary(chip4017.getOut(), resetQP);
-        chip555.pulse();
-        chip4017.shift();
+        runBoard board(chip555, chip4017, resetQP);
+        board.run();
+
+    } catch(const std::exception& e) {
+        ErrorMsg::CriticalError(e.what());
     }
 
     return 0;
